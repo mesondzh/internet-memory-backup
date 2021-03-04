db4c3aaeab85edb3e48c33ff461c6773bf5ffe2e
mensondzh <33677811+mesondzh@users.noreply.github.com>
2021-03-04 08:53:37 +0800

    backup tweets

--- run.py
+++ run.py
@@ -15,11 +15,12 @@ twitter_config = config_all["twitter"]
 
 if __name__ == "__main__":
     # twitter
+    now = datetime.now()
     my_tweets_file = "post/twitter/{}-{}-{}-my-tweets.md".format(now.year, now.month, now.day)
     backup_tweets(twitter_config, my_tweets_file)
 
-    # csdn
-    csdn.spider("ds19991999", "utils/cookie.txt", "post/csdn")
-
-    # typoche
-    get_article("post/typecho")
+    # # csdn
+    # csdn.spider("ds19991999", "utils/cookie.txt", "post/csdn")
+    #
+    # # typoche
+    # get_article("post/typecho")
